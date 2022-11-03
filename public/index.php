f98862ea60cce5183fe5bac87b06a5976db1f5a6
Enriko <root@6VSK-205-PORT1.localdomain>
2022-11-03 11:38:01 +0200

    Cleaned up index file

--- public/index.php
+++ public/index.php
@@ -6,8 +6,3 @@ spl_autoload_register(function ($class){
     require_once __DIR__ . '/../' . lcfirst(str_replace('\\' , '/' , $class)) .'.php';
 });
 
-use Public\Invoices\Subscription\Bill as SubBill;
-
-$subBill = new SubBill();
-
-var_dump($subBill);
