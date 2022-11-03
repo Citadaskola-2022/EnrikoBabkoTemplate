3558a6f9661e6371fe43b9ccac0d171e2d1061de
Enriko <root@6VSK-205-PORT1.localdomain>
2022-11-03 14:25:46 +0200

    autoload

--- public/index.php
+++ public/index.php
@@ -3,10 +3,7 @@
 declare(strict_types=1);
 
 use App\ServiceExpenses;
-
-spl_autoload_register(function ($class) {
-    require_once __DIR__ . '/../' . lcfirst(str_replace('\\', '/', $class)) . '.php';
-});
+require __DIR__ . '/../bootstrap/app.php';
 
 $transaction = new ServiceExpenses(30);
 
