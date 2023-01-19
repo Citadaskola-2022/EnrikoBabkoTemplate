6a62fec4dcf856f889a0b367af7ea2ecc0f4cf2a
Enriko <root@6VSK-205-PORT1.localdomain>
2023-01-19 12:14:51 +0200

    Episode 6

---
+++ VuePractice/Components/Customized.js
@@ -0,0 +1,6 @@
+import CustomizedButton from "./CustomizedButton.js";
+export default {
+    components: {
+        'customized-button': CustomizedButton
+    },
+};
--- VuePractice/Components/CustomizedButton.js
+++ VuePractice/Components/CustomizedButton.js
@@ -1,13 +1,26 @@
 export default {
         template:`
-            <button class="bg-green-200 px-4 py-3 disabled:cursor-not-allowed" :disabled="processing"
-                style="color:DarkBlue">
+            <button 
+                :class="{
+                    'bg-green-400 px-4 py-3 disabled:cursor-not-allowed': true,
+                    'bg-gray-200': type === 'secondary',
+                    'is-loading': processing
+                }" 
+                :disabled="processing"
+                style="color:DarkBlue"
+                >
                 <slot />
             </button>
             `,
-        data() {
-            return {
-                processing: false
-            };
-        }
+        props: {
+            type: {
+                type: String,
+                default: 'primary'
+            },
+            processing: {
+                type: Boolean,
+                default: false
+            }
+        },
+
 }
